a23db709d1dc4b5e3eb20e01a5c059bf6c387f25
npfvr1 <attila.tourypuel@gmail.com>
2024-01-16 22:49:51 +0100

    made the predict script callable from shell

--- mlops-project-26/src/predict_model.py
+++ mlops-project-26/src/predict_model.py
@@ -1,10 +1,22 @@
-import os
+import argparse
+
 import torch
-import torchvision.transforms as transforms
 from torchvision.datasets import ImageFolder
-import torchvision.models as models
-import torch.nn as nn
-import matplotlib.pyplot as plt
+
+from models.model import ResNet
+from data.make_dataset import get_transform
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('model',
+                    action="store",
+                    help="Path to pretrained model file (.pth)")
+# The images must be inside a subfolder inside a folder because we use ImageFolder
+parser.add_argument('img',
+                    action="store",
+                    help="Path to image folder (image format .jpg, RGB)")
+args = parser.parse_args()
+
 
 def get_default_device():
     """Pick GPU if available, else CPU"""
@@ -13,53 +25,43 @@ def get_default_device():
     else:
         return torch.device('cpu')
 
+
 def to_device(data, device):
     """Move tensor to the chosen device"""
     if isinstance(data, (list, tuple)):
         return [to_device(x, device) for x in data]
     return data.to(device, non_blocking=True)
 
-def predict_image(img, model, dataset):
+
+def predict(images, model, dataset):
+    predicted_labels = []
     device = get_default_device()
     model = to_device(model, device)
-    xb = to_device(img.unsqueeze(0), device)
-    yb = model(xb)
-    _, preds = torch.max(yb, dim=1)
-    return dataset.classes[preds[0].item()]
+    # Turn off gradients for prediction
+    with torch.no_grad():
+        # Set model to evaluation mode (faster inference)
+        model.eval()
+        for img in images:
+            xb = to_device(img.unsqueeze(0), device)
+            yb = model(xb)
+            _, preds = torch.max(yb, dim=1)
+            predicted_labels.append(dataset.classes[preds[0].item()])
+    return predicted_labels
+
 
 def main():
-    # Look into the directory
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    data_dir = os.path.join(current_dir, 'data', 'processed')
-    classes = os.listdir(data_dir + "/train_dataset.pt")
-
-    # Data transforms (normalization & data augmentation)
-    transformations = transforms.Compose([
-        transforms.Resize((128, 128)),
-        transforms.ToTensor(),
-    ])
-
     # PyTorch dataset
-    dataset = ImageFolder(data_dir+'/train_dataset.pt', transform=transformations)
+    dataset = ImageFolder(r'data\raw\train')
 
     # Load pre-trained model
-    model = models.resnet50(pretrained=True)
-    num_ftrs = model.fc.in_features
-    model.fc = nn.Linear(num_ftrs, 525)
+    model = ResNet()
+    model.load_state_dict(torch.load(args.model))
 
-    # Load model weights
-    model_path = './train_model'
-    model.load_state_dict(torch.load(model_path))
+    # Load the data to predict and process it in the same way as the training data
+    images_to_predict = [i for i, _ in ImageFolder(args.img, transform=get_transform())]
+    labels = predict(images_to_predict, model, dataset)
+    print(labels)
 
-    # Set model to evaluation mode
-    model.eval()
-
-    # Example usage
-    test_dataset = ImageFolder(data_dir+'/test_dataset.pt', transform=transformations)
-    img, label = test_dataset[100]
-    plt.imshow(img.permute(1, 2, 0))
-    predicted_label = predict_image(img, model, dataset)
-    print('Label:', dataset.classes[label], ', Predicted:', predicted_label)
 
 if __name__ == '__main__':
     main()
