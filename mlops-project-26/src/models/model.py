1cb660b850d0e347ca358e9d44eae1419661e334
npfvr1 <attila.tourypuel@gmail.com>
2024-01-16 19:08:40 +0100

    corrected the architecture of the model to 118 classes

--- mlops-project-26/src/models/model.py
+++ mlops-project-26/src/models/model.py
@@ -42,7 +42,7 @@ class ResNet(ImageClassificationBase):
         self.network = models.resnet50(pretrained=True)
         # Replace last layer
         num_ftrs = self.network.fc.in_features
-        self.network.fc = nn.Linear(num_ftrs, 100)
+        self.network.fc = nn.Linear(num_ftrs, 118)
     
     def forward(self, xb):
         return torch.sigmoid(self.network(xb))
